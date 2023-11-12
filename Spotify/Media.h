977ec8469bbfbc17bd66328fd35e57a5a93b2972
cosmincalinov <cosmin.calinov@yahoo.com>
2023-11-12 14:32:58 +0200

    Added constructor, copy constructor, destructor, = operator to media.

--- Spotify/Media.h
+++ Spotify/Media.h
@@ -1,10 +1,21 @@
 #pragma once
 #include<iostream>
+#include<string>
 
 class Media{
 private:
-	int duration;
+	const int duration = 0;
+	std::string artist;
 public:
+	Media(int _duration) : duration(_duration) { std::cout << "constr media" << std::endl; }
+	Media(const Media& src) { std::cout << "copy media" << std::endl; artist = src.artist; }
+	~Media() { std::cout << "destr media" << std::endl; }
+	Media& operator=(Media& alt_obj) {
+		if (this != &alt_obj) {
+			artist = alt_obj.artist;
+		}
+		return *this;
+	}
 	virtual void play() = 0;
 	virtual void pause() = 0;
 	virtual void addToQueue() = 0;
