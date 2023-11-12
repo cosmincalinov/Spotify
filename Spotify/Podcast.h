5011b33d309b8c4e4786bd86dea0113d1915e545
cosmincalinov <cosmin.calinov@yahoo.com>
2023-11-12 14:41:35 +0200

    Added constructors for the Podcast class.

--- Spotify/Podcast.h
+++ Spotify/Podcast.h
@@ -5,7 +5,19 @@
 class Podcast : public Media{
 private:
 	int duration;
+	std::string artist;
 public:
+	Podcast(int _duration) : Media(_duration) { std::cout << "constr pod" << std::endl; }
+	Podcast(const Media& src) : Media(src) { std::cout << "copy pod" << std::endl; artist = src.artist; }
+	~Podcast() { std::cout << "destr pod" << std::endl; }
+
+	Podcast& operator=(Podcast& alt_obj) {
+		if (this != &alt_obj) {
+			artist = alt_obj.artist;
+		}
+		return *this;
+	}
+
 	void play();
 	void pause();
 	void addToQueue();
