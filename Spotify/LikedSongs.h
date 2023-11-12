af267f1f6d9ebbd509dacea3ffe5e6f0d386ce91
cosmincalinov <cosmin.calinov@yahoo.com>
2023-11-12 15:02:50 +0200

    Added constructors for LikedSongs.

--- Spotify/LikedSongs.h
+++ Spotify/LikedSongs.h
@@ -5,9 +5,20 @@
 class LikedSongs : public Playlist{
 private:
 	int numberOfSongs = 0;
+	std::string name = "Liked Songs";
 public:
 	void shuffle();
 
+	LikedSongs(std::string _name) : Playlist(_name) { std::cout << "constr liked"; }
+	LikedSongs(const LikedSongs& src) : Playlist(src) { std::cout << "copy liked"; name = src.name; numberOfSongs = src.numberOfSongs; }
+	~LikedSongs() { std::cout << "destr liked"; }
+
+	LikedSongs& operator=(LikedSongs& src) {
+		if (this != &src) {
+			numberOfSongs = src.numberOfSongs;
+		}
+		return *this;
+	}
 };
 
 void LikedSongs::shuffle() {
