c7109949c881d97006a16b447816d3aab19d7f87
cosmincalinov <cosmin.calinov@yahoo.com>
2023-11-12 14:50:14 +0200

    Added constructors for the Playlist class.

--- Spotify/Playlist.h
+++ Spotify/Playlist.h
@@ -5,7 +5,20 @@
 class Playlist{
 private:
 	int numberOfSongs = 0;
+	std::string name;
 public:
+	Playlist(std::string _name) : name(_name) { std::cout << "constr play"; }
+	Playlist(const Playlist& src) { std::cout << "copy play"; name = src.name; numberOfSongs = src.numberOfSongs; }
+	~Playlist() { std::cout << "destr play"; }
+
+	Playlist& operator=(Playlist& src) {
+		if (this != &src) {
+			name = src.name;
+			numberOfSongs = src.numberOfSongs;
+		}
+		return *this;
+	}
+
 	void shuffle();
 };
 
